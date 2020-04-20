b4c395bfc8045b8a4abd67d68f9fd6b0dca20c11
flavio346 <flavioborges631@gmail.com>
2020-04-20 13:14:26 -0300

    subindo o app estilizado

--- gestao_validades/settings.py
+++ gestao_validades/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['gestao-validades.herokuapp.com']
+ALLOWED_HOSTS = ['gestao-validades.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -39,8 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'Validades',
     'home',
+
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,5 @@ LOGIN_URL = '/login/'
 LOGIN_REDIRECT_URL = 'lista'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
