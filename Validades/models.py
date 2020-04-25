10373c50304212f67b0c30a98d8dcb93843ea9b4
flavio346 <flavioborges631@gmail.com>
2020-04-24 22:30:33 -0300

    resolvendo bug2

--- Validades/models.py
+++ Validades/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Cadastro(models.Model):
     Produto = models.CharField(max_length=40)
-    Fornecedor = models.CharField(max_length=10)
+    Fornecedor = models.CharField(max_length=20)
     Quantidade = models.IntegerField()
     Data_do_Registro = models.DateField()
     Validade = models.DateField()
